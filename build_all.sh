7e05568cc43b6aeb0fca433a829b765e39557ee8
VitHrncir <vit@laskakit.cz>
2026-02-03 11:47:45 +0000

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # space separated values
-BOARDS="espink-v3 espink-v2 uespink-v1"
+BOARDS="espink-v2"
 DISPLAYS=$(grep '^#.*DISPLAY_' include/displays.hpp | cut -d '_' -f 2 | tr '\n' ' ')NONE
 # DISPLAYS="GDEY075T7 GDEY075Z08 GDEY1248F51 GDEM102F91 GDEM075F52"
 
